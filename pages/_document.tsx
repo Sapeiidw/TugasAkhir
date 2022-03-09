4837c49daef0370363a7d698789ba214791b6881
Sapeiidw <sapeiidw@gmail.com>
2022-03-09 14:24:18 +0800

    add Inter font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet"/> 
+      </Head>
       <body className='bg-white text-gray-900 dark:bg-gray-900 dark:text-white'>
         <Main />
         <NextScript />
