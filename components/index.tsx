903b76c5923935b264967c3a3b5f6ceed1e70063
Sapeiidw <sapeiidw@gmail.com>
2022-03-11 23:51:32 +0800

    masukkan component counter kedalam index component

--- components/index.tsx
+++ components/index.tsx
@@ -1,2 +1,3 @@
 export {default as ButtonDanger} from './Buttons/ButtonDanger'
 export {default as ButtonPrimary} from './Buttons/ButtonPrimary'
+export {default as Counter} from './Counter'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Counter from '../components/Counter'
+import {Counter} from '../components'
 
 const Home: NextPage = () => {
   return (
