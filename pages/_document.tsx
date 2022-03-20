855dffc9af11a3355e0be48109fbfef81b99cde4
Sapeiidw <sapeiidw@gmail.com>
2022-03-21 04:28:06 +0800

    fix bug crossOrigin fonts google

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&family=Montserrat&display=swap" rel="stylesheet" />
       </Head>
       <body className='bg-white text-gray-900 dark:bg-gray-900 dark:text-white'>
