52bad85ac8e7976505f4a1dc58162d078734b316
Sapeiidw <sapeiidw@gmail.com>
2022-03-21 04:30:34 +0800

    Membuat Navigation Component

---
+++ components/Navigations/Navigation.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import React from 'react'
+import style from '../../styles/Navigation.module.css'
+
+type Props = {}
+
+const Navigation: React.FC<Props> = (props) => {
+  return (
+    <>
+        <nav className={style.nav}>
+            <div className="container flex-row-center justify-between">
+                    <img src="/icons/InPOSery.svg" alt="logo" className='logo'/>
+                    <div className={style.navLinks}>
+                        <Link href=""><a className={style.navItem}>Home</a></Link>
+                        <Link href=""><a className={style.navItem}>Feature</a></Link>
+                        <Link href=""><a className={style.navItem}>Review</a></Link>
+                        <Link href=""><a className={style.navItem}>Pricing</a></Link>
+                    </div>
+                    <Link href=""><a className='btn btn-link'>Masuk</a></Link>`
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navigation
