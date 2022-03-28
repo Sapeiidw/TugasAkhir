7554078a3aa02e02d5f142ff6d3a68ff9a18207c
Sapeiidw <sapeiidw@gmail.com>
2022-03-28 13:56:24 +0800

    merapikan halaman forgot password

--- pages/auth/forgot-password.tsx
+++ pages/auth/forgot-password.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button, Input } from '../../components'
-import Form from '../../components/Forms/Form'
 import style from '../../styles/Auth.module.css'
 
 type Props = {}
@@ -15,12 +14,12 @@ const ForgotPassword: React.FC<Props> = (props) => {
                     <div className={style.img}>
                         <Image src="/images/Currency Crush Security.svg" layout='fill' />
                     </div>
-                    <div className="text">
-                        <h1 className='title'>Forgot your password?</h1>
-                        <p className='subtitle'>No worries, we’ll send you reset instruction</p>
+                    <div className={style.forgotPasswordText}>
+                        <h1 className={style.forgotPasswordTitle}>Forgot your password?</h1>
+                        <p className={style.forgotPasswordSubtitle}>No worries, we’ll send you reset instruction</p>
                     </div>
                     <form action="" className={style.forgotPasswordForm}>
-                        <Input type='text' name='email' placeholder='Email' />
+                        <Input type='text' name='email' placeholder='Your Email' />
                         <Button type="btn-primary" text="Send me an email" />
                     </form>
                     <Link href="/auth/signin"><a>Back to Login</a></Link>
