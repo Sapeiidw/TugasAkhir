b654a59b05ea857d19520fd58dd7af5e74657803
Sapeiidw <sapeiidw@gmail.com>
2022-04-01 04:40:03 +0800

    add transition on faq answer

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -20,7 +20,7 @@ const Dropdown: React.FC<Props> = (props) => {
             <span></span>
           </div>
         </div>
-        {open && <div className={style.faqAnswer}>{props.answer}</div>}
+        <div className={`${style.faqAnswer} ${open && style.faqAnswerOpen}`}>{props.answer}</div>
       </li>
     </>
   )
