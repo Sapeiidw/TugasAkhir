4d0501bb520df72d6cb5821c6f4cc935f74812a0
Sapeiidw <sapeiidw@gmail.com>
2022-07-03 20:58:28 +0800

    progres kasir

---
+++ pages/kasir/test.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { Product } from "../../types";
+import { Order, OrderIdItems } from "../../types/interfaces/Pos/Order";
+
+type Props = {};
+
+const test = (props: Props) => {
+  const [orderId, setOrderId] = useState<number>(0);
+  const [order, setOrder] = useState<Array<Order>>([]);
+  const [products, setProducts] = useState<Array<Product>>([]);
+  const [orderProducts, setOrderProducts] = useState<Array<any>>([{}]); // [{orderId: 1, productId: [1,2,3]}, {orderId: 2, productId: [1,2,3]}, ...]
+  return (
+    <div>
+      <button>tambah product</button>
+    </div>
+  );
+};
+
+export default test;
