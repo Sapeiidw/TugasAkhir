22879c7f695f540a9622d68005e769e540cfa414
Sapeiidw <sapeiidw@gmail.com>
2022-07-03 21:12:07 +0800

    progres kasir

--- pages/kasir/test.tsx
+++ pages/kasir/test.tsx
@@ -5,10 +5,10 @@ import { Order, OrderIdItems } from "../../types/interfaces/Pos/Order";
 type Props = {};
 
 const test = (props: Props) => {
-  const [orderId, setOrderId] = useState<number>(0);
-  const [order, setOrder] = useState<Array<Order>>([]);
-  const [products, setProducts] = useState<Array<Product>>([]);
-  const [orderProducts, setOrderProducts] = useState<Array<any>>([{}]); // [{orderId: 1, productId: [1,2,3]}, {orderId: 2, productId: [1,2,3]}, ...]
+  // const [orderId, setOrderId] = useState<number>(0);
+  // const [order, setOrder] = useState<Array<Order>>([]);
+  // const [products, setProducts] = useState<Array<Product>>([]);
+  // const [orderProducts, setOrderProducts] = useState<Array<any>>([{}]); // [{orderId: 1, productId: [1,2,3]}, {orderId: 2, productId: [1,2,3]}, ...]
   return (
     <div>
       <button>tambah product</button>
